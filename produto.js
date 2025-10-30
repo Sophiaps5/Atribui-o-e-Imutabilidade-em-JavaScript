28c0ec93f977b0dbfc9a7a868a7671e1e2ced8ad
Sophiaps5 <sophia.oliveira.silva02@escola.pr.gov.br>
2025-10-29 21:05:57 -0300

    Add files via upload

---
+++ produto.js
@@ -0,0 +1,9 @@
+const NomeDoProduto="Camisa de Algodão";
+let precoDoProduto=29.99;
+const marcaDoProduto="Marca X"
+const produtoCompleto=(NomeDoProduto+":"+"\n"+marcaDoProduto+":"+"\n"+precoDoProduto);
+precoDoProduto=49.00
+console.log(produtoCompleto)
+console.log("Produto:", produtoCompleto);
+console.log("Preço atualizado: R$", precoDoProduto);
+NomeDoProduto="Camiseta de Linho"
